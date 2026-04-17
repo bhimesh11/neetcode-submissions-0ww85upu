876700c10922b9f6331e6a04c13a5720652bffb0
Bhimesh Ganji <143391993+bhimesh11@users.noreply.github.com>
2026-04-17 10:58:41 -0400

    Add: median-of-two-sorted-arrays - submission-3

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-3.java
@@ -0,0 +1,87 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+// int[] c = new int[nums1.length+nums2.length];
+//         System.arraycopy(nums1,0,c,0,nums1.length);
+//        System.arraycopy(nums2,0,c,nums1.length,nums2.length);
+
+//         Arrays.sort(c);
+//         int low =0;
+//         int high = c.length-1;
+//         double median =0;
+//         int n = c.length;
+
+//        if(c.length%2!=0)
+//        {
+//         median = c[n/2];
+//        }else{
+//         median = (c[(n / 2) - 1] + c[n / 2]) / 2.0;
+//        // median = (c[(n / 2) - 1] + c[n / 2]) / 2.0;
+//        }
+//         return median;
+
+int m = nums1.length;
+int n = nums2.length;
+//always check with smaller array
+if(m>n)
+{
+    return findMedianSortedArrays(nums2,nums1);
+}
+//binary search
+int low =0;
+int high = m;
+//using this to partion how many left should be there in ledt side of the array 
+int left = (m+n+1)/2;
+
+while(low<=high)
+{
+    int mid1 = (low+high)/2;
+    int mid2 = left -mid1;
+    /*
+    intilize l1 l2 with min as they left element should be miniumum
+        intilize r1 r2 with max as they right element should be maximum
+
+    */
+    int l1 = Integer.MIN_VALUE;
+    int l2 = Integer.MIN_VALUE;
+    int r1 = Integer.MAX_VALUE;
+    int r2 = Integer.MAX_VALUE;
+//check and assign r1 from mid1 because mid1 might be might not be availle as we are nums.length -1
+     if(mid1<m)
+     {
+    r1 = nums1[mid1];
+    }
+    //check and assign r1 from mid1 because mid1 might be might not be availle as we are nums.length -1
+
+     if(mid2<n)
+     {
+r2 =  nums2[mid2];
+    }
+    
+     if(mid1-1>=0)
+     {
+        l1 = nums1[mid1-1];
+     }
+     if(mid2-1>=0)
+     {
+          l2 = nums2[mid2-1];
+   }
+    if(l1<=r2 && l2<=r1)
+    {
+        if((m+n)%2==0)
+        {
+             return ((double) Math.max(l1,l2) + Math.min(r1,r2))/2.0;
+        }else{
+          return Math.max(l1,l2);
+        }
+    }else if(l1>r2)
+    {
+        high = mid1-1;
+    }else{
+        low = mid1+1;
+    }
+}
+throw new IllegalArgumentException("Input arrays are not sorted");
+    }
+     
+}
